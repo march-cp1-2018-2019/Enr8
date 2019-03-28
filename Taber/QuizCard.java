75afbeeb15840db4b3d414da8e4446ca340c0197
MorganSkye <46610639+MorganSkye@users.noreply.github.com>
2019-03-28 15:44:31 -0500

    Add files via upload

---
+++ Taber/QuizCard.java
@@ -0,0 +1,37 @@
+
+
+import java.io.*;
+
+
+
+public class QuizCard {
+
+	String question;
+	String answer;
+
+
+
+	public QuizCard(String a, String b) {
+
+		question = a;
+		answer = b; 
+
+	}
+
+
+
+	public String setQuestion() {
+		return question;
+
+	}
+
+
+
+	public String setAnswer() 
+		return answer;
+
+	}
+
+
+
+}
