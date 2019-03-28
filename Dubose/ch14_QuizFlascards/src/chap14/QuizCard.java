9e0f9700329c1c4cfe630623e05fa1cf00acaabd
lost-space <46610655+lost-space@users.noreply.github.com>
2019-03-28 16:05:32 -0500

    Add files via upload

---
+++ Dubose/ch14_QuizFlascards/src/chap14/QuizCard.java
@@ -0,0 +1,31 @@
+package chap14;
+import java.io.*;
+
+public class QuizCard implements Serializable {
+
+	public QuizCard(String text, String text2) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getQuestion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public String setQuestion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+	public String getAnswer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public String setAnswer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}     
+
