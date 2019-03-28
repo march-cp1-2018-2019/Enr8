f9d2daf8fe9340446429f19e59a4cd87882f1ec2
shawdawg <46798120+shawdawg@users.noreply.github.com>
2019-03-28 18:18:57 -0500

    Add files via upload

---
+++ Russell/ch14_QuizFlascards/src/chap14/QuizCard.java
@@ -0,0 +1,28 @@
+package chap14;
+import java.io.*;
+
+//Stephen Russell 3/28/19
+
+public class QuizCard implements Serializable {
+
+	public QuizCard(String text, String text2) {
+	}
+
+	public String getQuestion() {
+		return null;
+	}
+	
+	public String setQuestion() {
+		return null;
+	}
+
+	public String getAnswer() {
+		return null;
+	}
+	
+	public String setAnswer() {
+		return null;
+	}
+	
+
+}     
