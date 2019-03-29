98ef4fe7f79083e58c54b6864441fd990bd27948
nortoha <39999692+nortoha@users.noreply.github.com>
2019-03-29 11:51:21 -0500

    TEACHER NOTES:
    Just missing the getters and then take a look at my notes for the setters.
    Good job

--- Taber/QuizCard.java
+++ Taber/QuizCard.java
@@ -19,7 +19,12 @@ public class QuizCard {
 	}
 
 
-
+	//TEACHER NOTES: this is so close!!! remember a setter sets the value, and a getter returns a value!
+	// this is a setter so it should set the value like this:
+	//public void setQuestion(String s){
+	//	this.question = s;
+	//}
+	
 	public String setQuestion() {
 		return question;
 
@@ -32,6 +37,10 @@ public class QuizCard {
 
 	}
 
+	//TEACHER NOTES: Still need getters here to return the value of question and answer.  like this:
+	//public String getQuestion(){
+	//	return question;
+	//}
 
 
 }
