1b38b440d7142f961cdd819f1c3ab0d817d4945e
nortoha <39999692+nortoha@users.noreply.github.com>
2019-03-29 11:54:40 -0500

    TEACHER NOTES

--- Russell/ch14_QuizFlascards/src/chap14/QuizCard.java
+++ Russell/ch14_QuizFlascards/src/chap14/QuizCard.java
@@ -5,17 +5,24 @@ import java.io.*;
 
 public class QuizCard implements Serializable {
 
+	//TEACHER NOTES: You are missing your instance variables for question and answer to go with your getters and setters below
+	
 	public QuizCard(String text, String text2) {
+		// TEACHER NOTES: inside this constructor you need to test text, and text2 to something.
 	}
 
 	public String getQuestion() {
+		//TEACHER NOTES: this should return the instance var question value not null
 		return null;
 	}
 	
 	public String setQuestion() {
+		//TEACHER NOTES: this should take a parameter inside the parenthesis for setting the question value
+		// then set that value to your instance variable for question.  Should not return anything.
 		return null;
 	}
 
+	//TEACHER NOTES: same for the answer too.
 	public String getAnswer() {
 		return null;
 	}
