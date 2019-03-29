d9e09e2c04d1c0c748c7f19412565d322add5990
nortoha <39999692+nortoha@users.noreply.github.com>
2019-03-29 11:45:28 -0500

    TEACHER NOTES

--- Dubose/ch14_QuizFlascards/src/chap14/QuizCard.java
+++ Dubose/ch14_QuizFlascards/src/chap14/QuizCard.java
@@ -3,21 +3,27 @@ import java.io.*;
 
 public class QuizCard implements Serializable {
 
+	//TEACHER NOTES: You are missing your instance variables for question and answer to go with your getters and setters below
+	
 	public QuizCard(String text, String text2) {
 		// TODO Auto-generated constructor stub
+		// TEACHER NOTES: inside this constructor you need to test text, and text2 to something.
 	}
 
 	public String getQuestion() {
 		// TODO Auto-generated method stub
+		//TEACHER NOTES: this should return the question value
 		return null;
 	}
 	public String setQuestion() {
 		// TODO Auto-generated method stub
+		//TEACHER NOTES: this should take a parameter inside the parenthesis for setting the question value
+		// then set that value to your instance variable for question.
 		return null;
 	}
 
 	
-	
+	//TEACHER NOTES: same for the answer too.
 	public String getAnswer() {
 		// TODO Auto-generated method stub
 		return null;
